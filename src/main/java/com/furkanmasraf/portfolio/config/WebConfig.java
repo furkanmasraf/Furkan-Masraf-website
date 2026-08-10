9bbfecda70b820f3eaa672cadaba4081f2a3efde
Furkan Masraf <masraffurkan@gmail.com>
2026-08-11 01:58:38 +0300

    fix(cors): update allowed origin URL formatting

--- src/main/java/com/furkanmasraf/portfolio/config/WebConfig.java
+++ src/main/java/com/furkanmasraf/portfolio/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "https://furkan-masraf-web-react.vercel.app",
+                        "https://furkanmasraf.vercel.app",
                         "http://localhost:5173",
                         "http://localhost:3000"
                 )
