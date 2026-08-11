339beb698d502391ffd9232ee3ba99e96335c679
Furkan Masraf <masraffurkan@gmail.com>
2026-08-11 19:23:21 +0300

    fix(security): sanitize application.properties, update lombok for JDK 21+ and add .env rules

--- src/main/java/com/furkanmasraf/portfolio/PortfolioApplication.java
+++ src/main/java/com/furkanmasraf/portfolio/PortfolioApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class
+PortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
